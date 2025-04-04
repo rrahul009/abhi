f48102f9201c00bfeb80db002cb773205b4e13db
rrahulkumaryadav347 <rrahulkumaryadav347@gmail.com>
2025-04-04 14:35:06 +0530

    added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import React from 'react';
+import Image from 'next/image'; // Import Image from next/image
 
 const AboutUs = () => {
   return (
@@ -20,16 +21,14 @@ const AboutUs = () => {
         </p>
       </div>
 
- 
       <div className="bg-white py-16 px-6 md:px-12">
         <div className="max-w-7xl mx-auto space-y-12">
-         
           <section className="text-center">
             <h2 className="text-3xl md:text-4xl font-semibold text-black mb-6">
               Our Mission
             </h2>
             <p className="text-lg text-gray-800 mb-6 leading-relaxed max-w-3xl mx-auto">
-              We strive to deliver eco-friendly cleaning solutions that make your space cleaner and healthier. Whether you're a wholesaler, retailer, or a business, our products provide efficient results from the very first use.
+              We strive to deliver eco-friendly cleaning solutions that make your space cleaner and healthier. Whether you&apos;re a wholesaler, retailer, or a business, our products provide efficient results from the very first use.
             </p>
             <p className="text-lg text-gray-800 mb-6 leading-relaxed max-w-3xl mx-auto">
               We are also working to expand our network and connect with corporate facilities, building management, malls, shopping complexes, and hospitals to offer our sustainable products to a wider audience.
@@ -42,7 +41,7 @@ const AboutUs = () => {
               Join Us on Our Journey
             </h3>
             <p className="text-lg text-white mb-6 leading-relaxed max-w-3xl mx-auto">
-              We're passionate about making a difference in the cleaning industry, and we need your support! Help us create a cleaner, healthier, and more sustainable world with our innovative cleaning solutions.
+              We&apos;re passionate about making a difference in the cleaning industry, and we need your support! Help us create a cleaner, healthier, and more sustainable world with our innovative cleaning solutions.
             </p>
             <button
               className="px-8 py-3 bg-amber-600 text-white font-semibold rounded-lg shadow-lg hover:bg-amber-700 transition duration-300 transform hover:scale-105"
